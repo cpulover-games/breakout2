95727752c34ce477b67c7e55769f8ad13279391e
Hieu Ngo <justacpulover@gmail.com>
2020-09-25 20:17:29 +0200

    First commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle1;
+    Vector3 paddleToBall;
+    bool gameStarted;
+    [SerializeField] float xPush=2f;
+    [SerializeField] float yPush=15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBall = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameStarted)
+        {
+            transform.position = paddle1.transform.position + paddleToBall; //stick ball to the paddle
+            LaunchOnClick();
+        }
+    }
+
+    private void LaunchOnClick()
+    {
+        if (Input.GetMouseButtonDown(0)) // left click
+        {
+            gameStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthUnit=16f;
+    [SerializeField] float minX=2f;
+    [SerializeField] float maxX=14f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosXInUnits = Mathf.Clamp(Input.mousePosition.x / Screen.width * screenWidthUnit, minX, maxX);
+        transform.position = new Vector2(mousePosXInUnits, transform.position.y);
+    }
+}
