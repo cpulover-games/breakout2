237d109f78af66df4539ce2d1c5ce88f39c4ae4f
Hieu Ngo <justacpulover@gmail.com>
2020-09-27 05:41:07 +0200

    Particle effect for destroying block

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,11 +6,16 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
     Level level;
+    [SerializeField] GameObject blockSparklesVFX;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
         level.UpdateBlockCollision();
+        // particle effect
+        GameObject sparkle = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkle, 1f);
+
     }
     // Start is called before the first frame update
     void Start()
