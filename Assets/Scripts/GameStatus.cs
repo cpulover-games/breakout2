87d2280fdaeba080d3df6a751c2e3c79321eb89c
Hieu Ngo <justacpulover@gmail.com>
2020-09-27 04:57:18 +0200

    Update score for next scene

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameStatus : MonoBehaviour
+{
+    [SerializeField] int score;
+    [SerializeField] int pointPerBlock = 5;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    private void Awake()
+    {
+        /* SINGLETON PATTERN */
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = 0.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateBlockCollision()
+    {
+        score += pointPerBlock;
+        scoreText.text = score.ToString();
+    }
+}
--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,30 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class Level : MonoBehaviour
 {
     [SerializeField] int breakableBlocks; // serialize for debugging
     SceneLoader sceneLoader; // cached ref
+    GameStatus gameStatus;
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed =1f;
-    [SerializeField] int score=0;
-    [SerializeField] int pointPerBlock=5;
-    [SerializeField] TextMeshProUGUI scoreText;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        gameStatus = FindObjectOfType<GameStatus>();
         sceneLoader = FindObjectOfType<SceneLoader>();
-        scoreText.text = 0.ToString();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        Time.timeScale = this.gameSpeed;
-        if (this.breakableBlocks == 0)
+        Time.timeScale = gameSpeed;
+        if (breakableBlocks == 0)
         {
             sceneLoader.loadNextScene();
         }
@@ -32,13 +31,12 @@ public class Level : MonoBehaviour
 
     public void CountBreakableBlocks()
     {
-        this.breakableBlocks++;
+        breakableBlocks++;
     }
 
     public void UpdateBlockCollision()
     {
-        this.breakableBlocks--;
-        score += pointPerBlock;
-        scoreText.text = score.ToString();
+        breakableBlocks--;
+        gameStatus.UpdateBlockCollision();
     }
 }
